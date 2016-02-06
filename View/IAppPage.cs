35d64bb6e0cec2f6639998d1024409ae8b955718
Captwalloper <comccoy@cfl.rr.com>
2016-02-06 01:18:12 -0500

    V_1.00
    Compiles!

---
+++ View/IAppPage.cs
@@ -0,0 +1,10 @@
+﻿using CortanaExtension.Shared.Utility.Cortana;
+using System.Threading.Tasks;
+
+namespace FlawlessCowboy.View
+{
+    public interface IAppPage
+    {
+        Task RespondToVoice(CortanaCommand command);
+    }
+}
